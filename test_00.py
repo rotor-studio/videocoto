ef77b3579d1408a14848d5c886f9790a72f050c3
rotorstudio <roman@rotor-studio.net>
2021-05-25 13:17:59 +0200

    OSC IN/OUT

--- test_00.py
+++ test_00.py
@@ -9,6 +9,10 @@ import logging
 logging.basicConfig(level=logging.INFO)
 from pythonosc import udp_client
 import argparse
+import math
+import threading
+from pythonosc import dispatcher
+from pythonosc import osc_server
 
 #global
 step = 0
@@ -19,6 +23,45 @@ portIn = 8000
 destIp = array('i')
 stepVideo = array('l')
 
+     
+#Prevencion de errores si no hay puertos de destino
+def handle_error(self, request, client_address):
+    """Handle an exception in the Server's callbacks gracefully.
+    Writes the error to sys.stderr and, if the error_prefix (see setSrvErrorPrefix()) is set,
+    sends the error-message as reply to the client
+    """
+    (e_type, e) = sys.exc_info()[:2]
+    self.printErr("%s on request from %s: %s" % (e_type.__name__, getUrlStr(client_address), str(e)))
+
+    if self.print_tracebacks:
+        import traceback
+        traceback.print_exc() # XXX But this goes to stderr!
+
+    if len(self.error_prefix):
+        self.sendOSCerror("%s: %s" % (e_type.__name__, str(e)), client_address)
+
+     
+def print_fader_handler(unused_addr, args, value):
+    print("[{0}] ~ {1:0.2f}".format(args[0], value))
+
+
+def print_xy_fader_handler(unused_addr, args, value1, value2):
+    print("[{0}] ~ {1:0.2f} ~ {2:0.2f}".format(args[0], value2, value1))
+
+#Listen OSC
+if __name__ == "__main__":
+     dispatcher = dispatcher.Dispatcher()
+     dispatcher.map("/stepTo", print)
+     
+
+def listenOsc(ip, port):
+    print("Starting Server")
+    server = osc_server.ThreadingOSCUDPServer(
+        (ip, port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    thread = threading.Thread(target=server.serve_forever)
+    thread.start()
+    
 
 #Read the config.txt
 conf = list(open("config.txt", "r"))
@@ -60,35 +103,34 @@ def config(mode):
 #Send Osc
 def sendOsc(destIp,destPort,now):
 
-   finalIp = "192.168.200."+str(destIp)
-   print("Ip de destino>> "+str(finalIp))
+   print("Ip de destino>> "+str(destIp))
    print("Puerto de destino>> "+str(destPort))
    print("Play video en destino>> "+str(now))
 
-   client = udp_client.SimpleUDPClient(finalIp,destPort)
+   client = udp_client.SimpleUDPClient(destIp,destPort)
+   client.print_tracebacks = True
    
    client.send_message("/stepTo",now)
    time.sleep(1)
    
-#Receive Osc
-def recOsc():
-     print("Msg")
-      
+
 #Display videos
-def playVideos():
+def playVideos(videoNow):
      #sleep(5)
      player = OMXPlayer(videoNow)
      player.play()
      player.quit()
 
 
+config(int(mode))
 readFolder(videoPth)
 readTimeline(csvPth)
-config(int(mode))
-#sendOsc(destIp[0],destPort,now)
+listenOsc("127.0.0.1",8000)
+
 
 print(destIp);
 print(stepVideo);
+sendOsc("127.0.0.1",8000,now)
 
 
     
